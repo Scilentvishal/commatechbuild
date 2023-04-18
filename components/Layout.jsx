5085ae671a27d4a7921910d1b0ca5a92aeb597a2
Vishal Rahangdale <vishalrahangdale14@gmail.com>
2023-04-18 10:25:07 +0530

    third commit

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import React from 'react'
 import Footer from './Footer'
 import Navbar from './Navbar'
+import NextNProgress from "nextjs-progressbar";
 
 const Layout = ({children}) => {
   return (
@@ -10,6 +11,8 @@ const Layout = ({children}) => {
       <link rel="icon" href="/commatechfav.png" />
       </Head>
         <Navbar />
+        <NextNProgress height={6} color="#07F300 
+" />
         {children}
         <Footer />
         </div>
